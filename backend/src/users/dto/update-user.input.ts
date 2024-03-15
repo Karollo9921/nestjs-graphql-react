07822e490a7fce69819bc996aead4ced470920f1
Karol Kluba <karol@mysmarthotel.com>
2024-03-15 17:41:13 +0100

    feat: add some validation and logging

--- backend/src/users/dto/update-user.input.ts
+++ backend/src/users/dto/update-user.input.ts
@@ -1,8 +1,10 @@
 import { CreateUserInput } from './create-user.input';
-import { InputType, Field, PartialType } from '@nestjs/graphql';
+import { InputType, Field, OmitType } from '@nestjs/graphql';
+import { IsMongoId } from 'class-validator';
 
 @InputType()
-export class UpdateUserInput extends PartialType(CreateUserInput) {
+export class UpdateUserInput extends OmitType(CreateUserInput, ['password']) {
   @Field()
+  @IsMongoId()
   id: string;
 }
